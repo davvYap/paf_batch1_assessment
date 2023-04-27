291956bf228a92a835038eb55d6c5a4bad2eb2ff
David Yap <davvyap@gmail.com>
2023-04-27 21:36:26 +0800

    Config for railway

--- batch1_assessment/src/main/resources/schema.sql
+++ batch1_assessment/src/main/resources/schema.sql
@@ -1,9 +1,13 @@
-CREATE TABLE `acme_bank`.'accounts' (
-  'account_id' INT NOT NULL,
-  'name' VARCHAR(45) NOT NULL,
-  'balance' DECIMAL(6,2) NOT NULL,
-  PRIMARY KEY ('account_id'));
+CREATE TABLE accounts (
+account_id VARCHAR(45) NOT NULL,
+name VARCHAR(45) NOT NULL,
+balance DECIMAL(10,2) NOT NULL,
+PRIMARY KEY (account_id));
 
 insert into accounts(account_id, name, balance)
 values
-(102595,"whostheboss",88888.88);
+("ABCDE09871","whostheboss",88888.88),
+("BSDWQ87652","david",88888.88),
+("LKIUH12398","carmen",88888.88),
+("LOKHS09621","tomas",88888.88),
+("SDHGB08932","yunice",88888.88)
